2098b9add5fad411ed40aec292aa20fb3df19b44
CAPITAL\cpnbe <cpnbetty@gmail.com>
2023-11-07 17:02:45 -0600

    Results of initial test

--- main.py
+++ main.py
@@ -1,37 +1,58 @@
 import os
 import shutil
 import json
-from openai import OpenAI
+import openai
 from PIL import Image
 import piexif
+import base64
+import requests
 
-# Set your OpenAI API key
-openai_api_key = 'your-api-key'
-client = OpenAI(api_key=openai_api_key)
+# Function to encode the image and get tags from OpenAI API
+def classify_image(image_path, api_key):
+    # Encode the image to base64
+    base64_image = encode_image(image_path)
 
-# Function to call OpenAI API and get image tags
-def classify_image(image_path):
-    with open(image_path, 'rb') as image_file:
-        image_data = image_file.read()
-    
-    response = client.chat.completions.create(
-        model="gpt-4-vision-preview",
-        messages=[
+    # Headers for the API request
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
+
+    # Payload for the API request
+    payload = {
+        "model": "gpt-4-vision-preview",
+        "messages": [
             {
                 "role": "user",
                 "content": [
-                    {"type": "text", "text": "Provide a list of tags describing this image."},
-                    {"type": "data", "data": image_data},
+                    {"type": "text", "text": "What’s in this image?"},
+                    {"type": "image_url", "image_url": f"data:image/jpeg;base64,{base64_image}"}
                 ],
             }
         ],
-        max_tokens=60
-    )
-    
-    # Assuming that the response will be a list of tags in text format
-    tags_text = response.choices[0].message['content']['text']
-    tags = tags_text.split(', ')  # Split tags by comma and space
-    return tags
+        "max_tokens": 300
+    }
+
+    # Make the API request
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+    # Handle the response
+    try:
+        response_data = response.json()
+        print("API Response:", response_data)  # Debug: Print the full response
+        # Parse the response to get the tags
+        tags_text = response_data['choices'][0]['message']['content']['text']
+        tags = tags_text.split(', ')  # Split tags by comma and space
+        return tags
+    except Exception as e:
+        print(f"Error processing the response: {e}")
+        return []
+
+# Helper function to encode image to base64
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
 
 # Function to embed tags into image metadata
 def embed_metadata(image_path, tags):
@@ -54,12 +75,12 @@ def move_to_output(image_path):
     shutil.move(image_path, os.path.join(output_folder, os.path.basename(image_path)))
 
 # Main processing loop for images in the Inputs folder
-def process_images(input_folder, output_folder):
+def process_images(input_folder, api_key):
     for image_name in os.listdir(input_folder):
         image_path = os.path.join(input_folder, image_name)
         if os.path.isfile(image_path):
             try:
-                tags = classify_image(image_path)
+                tags = classify_image(image_path, api_key)  # Pass the api_key argument here
                 embed_metadata(image_path, tags)
                 move_to_output(image_path)
                 print(f"Processed and moved: {image_name}")
@@ -69,7 +90,17 @@ def process_images(input_folder, output_folder):
 # Define the root, input, and output folders
 root_folder = os.getcwd()
 input_folder = os.path.join(root_folder, 'Inputs')
-output_folder = os.path.join(root_folder, 'Outputs')
+
+# Retrieve API key from environment variable
+api_key = os.getenv('OPENAI_API_KEY')  # Make sure the environment variable is set
+
+# Check if the API key is available
+if api_key:
+    # Process all images in the Inputs folder
+    process_images(input_folder, api_key)
+else:
+    print("API key not found. Please set the OPENAI_API_KEY environment variable.")
+
 
 # Process all images in the Inputs folder
-process_images(input_folder, output_folder)
+process_images(input_folder,api_key)
